3a76fadd71816c390dcf71c196ff98facad8c314
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2020-02-26 12:35:58 +0700

    Modifikasi Untuk Connect ke API, done

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Selamat Datang');
+app.get('/api/datasiswa', function (req, res) {
+   
+    var sql = require("mssql");
+    // config for your database
+    var config = {
+        user: 'su',
+        password: 'SaSa1212',
+        server: '10.199.13.253', 
+        database: 'nrp05111740000078' 
+    };
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+        var request = new sql.Request();
+        // query to the database and get the records
+        request.query('select Nama from Mahasiswa', function (err, recordset) {
+            if (err) console.log(err)
+            
+            console.log("berhasil")
+            res.send(recordset);
+        });
+    });
 });
 
-server.listen(8014, '10.199.14.46', () => {
+
+app.get('/', function (req, res) {
+  res.end("Selamat Datang,Implementasi API get di Sql server, request map api di '10.199.14.46:8014/api/datasiswa'");
+  // return "Implementasi API get di Sql server, request map api di '10.199.14.46:8014/api/datasiswa'"
 });
+
+var server = app.listen(8014, 'localhost', function () {
+});
+// server.listen(8014, '10.199.14.46', () => {
+// });
