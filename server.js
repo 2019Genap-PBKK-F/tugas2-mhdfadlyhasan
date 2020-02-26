f87f225a824f50d689a2596aa04a65c74aab83c7
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2020-02-26 10:54:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-var http = require("http");
-var fs = require('fs');
+var http = require('http');
 
-fs.readFile('./home/ridho/tugas2/05111740000078/index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);
-        response.end();  
-    }).listen(8014,'10.199.14.46',() => {
-        console.log(`Server running at locahost`)})
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Selamat Datang');
+});
+
+server.listen(8014, '10.199.14.46', () => {
 });
